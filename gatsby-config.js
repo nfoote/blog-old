a1622d7f7df46c1da7e5595b9e504eca65886e3a
Nick <nfoote@utas.edu.au>
2022-05-22 14:56:35 +1000

    remove rule

--- gatsby-config.js
+++ gatsby-config.js
@@ -111,14 +111,6 @@ module.exports = {
         ],
       },
     },
-      {
-        resolve: "gatsby-plugin-react-svg",
-        options: {
-          rule: {
-            include: /svg/
-          }
-        }
-      },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
