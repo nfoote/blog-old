bf335bb04148e9fbc719edfd974d154db9db872a
Nick <nfoote@utas.edu.au>
2022-06-12 13:03:24 +1000

    add check for window usages

--- src/hooks/useWindowDimensions.js
+++ src/hooks/useWindowDimensions.js
@@ -1,23 +1,29 @@
 import { useState, useEffect } from 'react';
 
 function getWindowDimensions() {
-  const { innerWidth: width, innerHeight: height } = window;
-  return {
-    width,
-    height
-  };
+  if (isBrowser) {
+    const { innerWidth: width, innerHeight: height } = window;
+    return {
+      width,
+      height
+    };
+  }
 }
+
+const isBrowser = () => typeof window !== "undefined"
 
 export default function useWindowDimensions() {
   const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
 
   useEffect(() => {
-    function handleResize() {
-      setWindowDimensions(getWindowDimensions());
+    if (isBrowser) {
+      function handleResize() {
+        setWindowDimensions(getWindowDimensions());
+      }
+  
+      window.addEventListener('resize', handleResize);
+      return () => window.removeEventListener('resize', handleResize);
     }
-
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
   }, []);
 
   return windowDimensions;
