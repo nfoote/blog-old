e1b09438e8895c5340670b3739794224fd49d42e
Nick <nfoote@utas.edu.au>
2022-06-16 21:11:31 +1000

    try adding a throttle to window resize

--- src/hooks/useWindowDimensions.js
+++ src/hooks/useWindowDimensions.js
@@ -18,19 +18,36 @@ const isBrowser = () => typeof window !== "undefined"
 
 export default function useWindowDimensions() {
   const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
-  //const [hasWindowDimensions, setHasWindowDimensions] = useState(false);
+  // const [hasWindowDimensions, setHasWindowDimensions] = useState(false);
+
+  const delay = 500
+  let throttled = false 
 
   useEffect(() => {
     if (isBrowser()) {
       function handleResize() {
         setWindowDimensions(getWindowDimensions());
       }
-  
-      window.addEventListener('resize', handleResize);
-      //setHasWindowDimensions(true);
+      // window.addEventListener('resize', handleResize);
+
+      // window.resize event listener
+      window.addEventListener('resize', function() {
+        // only run if we're not throttled
+        if (!throttled) {
+          // actual callback action
+          handleResize();
+          // we're throttled!
+          throttled = true;
+          // set a timeout to un-throttle
+          setTimeout(function() {
+            throttled = false;
+          }, delay);
+        }  
+      });
+
       return () => window.removeEventListener('resize', handleResize);
     }
-  }, []);
+  }, [windowDimensions]);
 
   return windowDimensions;
 }
