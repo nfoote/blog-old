7039c70bc4bd16c9e070441da2256f9f48c41ccb
Nick <nfoote@utas.edu.au>
2022-07-16 21:39:03 +1000

    refactor toggler to be in same componenet as burger

--- src/components/site-header.js
+++ src/components/site-header.js
@@ -1,29 +1,34 @@
-import React, { Fragment, useRef, useState } from 'react';
-import ThemeToggle from "./theme-toggle";
-import { Link } from 'gatsby'
-
+import React, { useRef, useState } from 'react';
+import { ThemeToggler } from 'gatsby-plugin-dark-mode'
 
 import Burger from "../components/menu/Burger/Burger"
 import Menu from "../components/menu/Menu/Menu"
 import { useOnClickOutside } from '../hooks/useOnClickOutside';
+import ToggleSwitch from './toggle-switch';
 
 const SiteHeader = props => {
-    const [open, setOpen] = useState(false);
-    const node = useRef(); 
-    useOnClickOutside(node, () => setOpen(false));
-    
-return(
-    <>
-        <h1 className={props.className}>
-            <ThemeToggle className="theme-toggle"  />
-        </h1>
+  const [open, setOpen] = useState(false);
+  const node = useRef(); 
+  useOnClickOutside(node, () => setOpen(false));
 
-        <div ref={node}>
+  return (
+    <>
+      <ThemeToggler>
+        {({ theme, toggleTheme }) => (
+          <h1 className={props.className}>
+          <ToggleSwitch
+              onThemeSwitch={e => toggleTheme(e ? 'dark' : 'light')} 
+              isChecked={theme === 'dark'} 
+          />
+          </h1>
+        )}
+      </ThemeToggler>
+      <div ref={node}>
           <Burger open={open} setOpen={setOpen} />
           <Menu open={open} />
-        </div>
+      </div>
     </>
- )
+  )
 }
 
 export default SiteHeader;
