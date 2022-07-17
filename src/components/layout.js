7a82dde979b9e06612264ca9304145e1c8f03cac
Nick <nfoote@utas.edu.au>
2022-07-17 11:00:21 +1000

    test fix for menu displaying on load

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,15 +4,17 @@ import SiteHeader from "./site-header"
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
   const isRootPath = location.pathname === rootPath
-  let header
+  // let header
 
-  header = isRootPath 
-    ? <SiteHeader title={title} className="main-heading" />
-    : <SiteHeader title={title} className="header-link-home" />
+  // header = isRootPath //fix
+  //   ? <SiteHeader title={title} className="main-heading" />
+  //   : <SiteHeader title={title} className="header-link-home" />
 
   return (
     <div className="global-wrapper" data-is-root-path={isRootPath}>
-      <header className="global-header">{header}</header>
+      <header className="global-header">
+      <SiteHeader title={title} className="main-heading" />
+      </header>
       <main>{children}</main>
     </div>
   )
